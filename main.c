2351750426404f674cabd9cd30067f0597098e1d
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-08-19 06:31:32 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ int i;
+    int T[10];
+
+    for(i=0;i<=9;i++){
+            T[i]=0;
+        printf("t[%d]=%d \n ",i,T[i]);
+    }
+    return 0;
+}
